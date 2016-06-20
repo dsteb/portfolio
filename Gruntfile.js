7c5caa7cc80702749a561e5f0284f23f01b658ce
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-06-20 19:46:46 +0200

    Add grunt responsive images task

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 1600,
+            suffix: '_large_2x',
+            quality: 30
+          }]
+        },
+        files: [{
+          expand: true,
+          cwd: 'images_src/',
+          src: ['*.{gif,jpg,png}'],
+          dest: 'images/'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+
+  grunt.registerTask('default', ['responsive_images']);
+};
